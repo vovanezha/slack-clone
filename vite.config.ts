d6d8b0d932336970b0211f8e0b2623eb5f661eb6
vovanezha <vova19tup@gmail.com>
2023-05-23 11:44:06 +0300

    copy mocks to public dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './mocks',
+          dest: 'public',
+        },
+      ],
+    }),
+  ],
   server: {
     port: 3000,
   },
